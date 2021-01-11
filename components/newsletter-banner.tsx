113e83ed98a1852169b83cb57b565bae994092b5
Tuomo Kankaanpää <kankaanpaa.tuomo@gmail.com>
2021-01-11 23:37:38 +0200

    Test different redirect url for newsletter

--- components/newsletter-banner.tsx
+++ components/newsletter-banner.tsx
@@ -21,7 +21,7 @@ const NewsletterBanner = () => {
         <input
           type="hidden"
           name="redirect"
-          value="https://www.tuomokankaanpaa.com/newsletter-confirmation-email"
+          value="https://tuomokankaanpaa.vercel.app/newsletter-confirmation-email"
         />
         <input type="hidden" name="meta_message" value="1" />
         <input type="hidden" name="meta_required" value="email" />
