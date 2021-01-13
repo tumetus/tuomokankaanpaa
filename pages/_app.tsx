8d4e8da5cd525662d006895f4e25ac991de7b176
Tuomo Kankaanpää <kankaanpaa.tuomo@gmail.com>
2021-01-13 22:57:21 +0200

    Add privacy policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { jsx, ThemeProvider } from "@emotion/react";
 import { AppProps } from "next/app";
+import Link from "next/link";
 import CookieConsent from "react-cookie-consent";
 import Meta from "../components/meta";
 import "../styles/global.css";
@@ -25,7 +26,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
       >
         This site uses cookies. By continuing to use this website, you agree to
-        their use.
+        their use. See{" "}
+        <Link href="/privacy">
+          <a>privacy policy</a>
+        </Link>{" "}
+        for more info.
       </CookieConsent>
     </ThemeProvider>
   );
