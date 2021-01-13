7212258d87f3dade525b91eea4903d849691c87c
Tuomo Kankaanpää <kankaanpaa.tuomo@gmail.com>
2021-01-13 22:47:02 +0200

    Add cookie consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { jsx, ThemeProvider } from "@emotion/react";
 import { AppProps } from "next/app";
+import CookieConsent from "react-cookie-consent";
 import Meta from "../components/meta";
 import "../styles/global.css";
 
@@ -14,6 +15,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Meta />
       <Component {...pageProps} />
+      <CookieConsent
+        style={{ background: "#000" }}
+        buttonStyle={{
+          color: "#fff",
+          fontSize: "13px",
+          background: "#0070f3",
+          fontWeight: "bold",
+        }}
+      >
+        This site uses cookies. By continuing to use this website, you agree to
+        their use.
+      </CookieConsent>
     </ThemeProvider>
   );
 }
