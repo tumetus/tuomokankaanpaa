13290c85d61c17012449293f8f4a7cf3ded2efcb
Tuomo Kankaanpää <kankaanpaa.tuomo@gmail.com>
2021-01-14 00:59:53 +0200

    Add content to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/css";
 import Image from "next/image";
+import Link from "next/link";
 
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import TwitterIcon from "@material-ui/icons/Twitter";
@@ -81,7 +82,111 @@ export default function Home() {
           </p>
         </div>
       </div>
+
+      <section>
+        <h2>I do Youtube videos</h2>
+        <p>
+          I do programming and web development videos on{" "}
+          <a
+            href="https://www.youtube.com/channel/UC34UXFLKqdW3cpk5CBu2Siw"
+            target="_blank"
+          >
+            Youtube
+          </a>
+          . This is currently my main focus when it comes to teaching new
+          things.
+        </p>
+        <div>
+          <a href="https://www.youtube.com/watch?v=uuXxomVFbC8" target="_blank">
+            <YtImage src="images/code-fast-vs-code-thumbnail.jpg" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=mqP_qNViGUQ" target="_blank">
+            <YtImage src="images/focus-element-in-react-thumbnail.jpg" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=9Jo084i0Dzs" target="_blank">
+            <YtImage src="images/best-vs-code-shortcuts-thumbnail.jpg" />
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <h2>I teach web development courses</h2>
+        <p>
+          I am a teacher at Udemy and Skillshare. I am currently teaching
+          courses on React, Draft.js, web development.
+        </p>
+        <p>
+          Check out the{" "}
+          <Link href="/courses">
+            <a>courses</a>
+          </Link>{" "}
+          page for more.
+        </p>
+      </section>
+
+      <section>
+        <h2>I write blog posts</h2>
+        <p>
+          I have written blog posts for past couple of years. I enjoy teaching
+          things I have learned through blog posts. Most of my posts are about
+          web development, React, Javascript and Draft.js.
+        </p>
+        <p>
+          Check out the{" "}
+          <Link href="/blog">
+            <a>blog</a>
+          </Link>{" "}
+          page for more.
+        </p>
+      </section>
+
+      <section>
+        <h2>I do mentoring</h2>
+        <p>
+          I am passionate about teaching and honestly just want to help people
+          out. Having someone give you constructive feedback or having someone
+          to spar your ideas with can really help you reach new levels on your
+          craft. That is why I also offer 1on1 mentoring sessions.
+        </p>
+        <p>Send me email at tuomo(a)tuomokankaanpaa.com and let's talk!</p>
+      </section>
+
+      <section>
+        <h2>I do freelance work</h2>
+        <p>
+          I am available for freelance work. If you need e.g. help or consulting
+          on a project, debugging help, React components or tutorial video on
+          specific topic, I am able to help.
+        </p>
+        <p>Send me email at tuomo(a)tuomokankaanpaa.com and let's talk!</p>
+      </section>
+
       <Footer />
     </Layout>
   );
 }
+
+const YtImage = ({ src }: { src: string }) => {
+  return (
+    <img
+      className={css(`
+        display: inline-block;
+        margin: 10px 10px 0 0;
+        width: 30%;
+
+        cursor: pointer;
+        opacity: 0.8;
+
+        :hover {
+          opacity: 1;
+        }
+
+        @media (max-width: ${SMALL}px) {
+          width: 48%;
+          margin: 0px 5px 0 0;
+        }
+      `)}
+      src={src}
+    />
+  );
+};
